f9e699cf0e74c54dee05af45dfaaf09f6aef188e
Maru <zhangbinhui@souche.com>
2017-07-16 15:20:08 +0800

    ref:[authorize] Append event hander

--- core/server/wxauthorize.py
+++ core/server/wxauthorize.py
@@ -29,19 +29,39 @@ class WxSignatureHandler(tornado.web.RequestHandler):
 
     def post(self):
         body = self.request.body
-        # logger.debug('微信消息回复中心】收到用户消息' + str(body.decode('utf-8')))
+        logger.debug('微信消息回复中心】收到用户消息' + str(body.decode('utf-8')))
         data = ET.fromstring(body)
         ToUserName = data.find('ToUserName').text
         FromUserName = data.find('FromUserName').text
         MsgType = data.find('MsgType').text
-        if MsgType == 'event':
+        if MsgType == 'text' or MsgType == 'voice':
+            '''文本消息 or 语音消息'''
+            try:
+                MsgId = data.find("MsgId").text
+                if MsgType == 'text':
+                    Content = data.find('Content').text  # 文本消息内容
+                elif MsgType == 'voice':
+                    Content = data.find('Recognition').text  # 语音识别结果，UTF8编码
+                if Content == u'你好':
+                    reply_content = '您好,请问有什么可以帮助您的吗?'
+                else:
+                    # 查找不到关键字,默认回复
+                    reply_content = "客服小儿智商不够用啦~"
+                if reply_content:
+                    CreateTime = int(time.time())
+                    out = self.reply_text(FromUserName, ToUserName, CreateTime, reply_content)
+                    self.write(out)
+            except:
+                pass
+
+        elif MsgType == 'event':
             '''接收事件推送'''
             try:
                 Event = data.find('Event').text
                 if Event == 'subscribe':
                     # 关注事件
                     CreateTime = int(time.time())
-                    reply_content = '欢迎关注我的公众号~'
+                    reply_content = self.sys_order_reply
                     out = self.reply_text(FromUserName, ToUserName, CreateTime, reply_content)
                     self.write(out)
             except:
